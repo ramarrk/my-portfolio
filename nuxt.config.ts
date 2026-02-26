e455c9e0f6f7dd93d350f590722e8391287118e7
M. Ramadhan Kurniawan <ramadhank421@gmail.com>
2026-02-26 07:30:25 +0700

    ganti icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 export default defineNuxtConfig({
+  
+  app: {
+    head: {
+      title: 'Portofolio Rama', 
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/seol.png' } 
+      ]
+    }
+  },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts' 
   ],
-  
 
   googleFonts: {
     families: {
